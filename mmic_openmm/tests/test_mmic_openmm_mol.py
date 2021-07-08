4e7ba78df927e9e2aa0ffb114c2a6103626ae88f
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-07-08 14:25:15 -0400

    create basic test for file read/write

---
+++ mmic_openmm/tests/test_mmic_openmm_mol.py
@@ -0,0 +1,59 @@
+"""
+Unit and regression test for the mmic_openmm package.
+"""
+
+# Import package, test suite, and other packages as needed
+import mmic_openmm
+import pytest
+import sys
+import os
+from simtk.openmm.app import (
+    PDBFile,
+    GromacsGroFile,
+    GromacsTopFile,
+    AmberPrmtopFile,
+    AmberInpcrdFile,
+)
+import mmelemental as mm
+import mm_data
+
+pdbfile = mm_data.mols["dialanine.pdb"]
+grofile = mm_data.mols["dialanine.gro"]
+inpcrdfile = mm_data.mols["dialanine.inpcrd"]
+topfile = mm_data.ffs["dialanine.top"]
+
+
+def test_mmic_openmm_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "mmic_openmm" in sys.modules
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_mmic_to_mol_from_pdb(**kwargs):
+    struct = PDBFile(pdbfile)
+    inputs = {"data_object": struct.topology, "kwargs": kwargs}
+    return mmic_openmm.components.OpenMMToMolComponent.compute(inputs)
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_mmic_to_mol_from_gro(**kwargs):
+    struct = GromacsGroFile(grofile)
+    top = GromacsTopFile("dialanine.top")
+    inputs = {"data_object": struct, "kwargs": kwargs}
+    return mmic_openmm.components.OpenMMToMolComponent.compute(inputs)
+
+
+def test_mol_to_openmm(**kwargs):
+    mmol = mm.models.molecule.mm_mol.Molecule.from_file(grofile, topfile)
+    inputs = {"schema_object": mmol}
+    return mmic_openmm.components.MolToOpenMMComponent.compute(inputs)
+
+
+def test_io_methods(**kwargs):
+    omol = mmic_openmm.models.OpenMMMol.from_file(pdbfile, topfile)
+    assert isinstance(omol.data, omol.dtype)
+
+    omol.to_file("tmp.pdb")
+    os.remove("tmp.pdb")
+    # mmol = omol.to_schema()
+    # assert isinstance(mmol, mm.models.molecule.Molecule)
