9c7efc48f37ddc7ee694b26fd483fb1e4db450b0
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-07-22 14:20:53 -0400

    complete nonbonded ter for OpenMMToFFComponent

--- mmic_openmm/components/ff_component.py
+++ mmic_openmm/components/ff_component.py
@@ -7,33 +7,14 @@ from mmic_translator.components import TransComponent
 from typing import List, Tuple, Optional
 from collections.abc import Iterable
 from mmelemental.util.units import convert
-import parmed
+from simtk.openmm.app import forcefield as openmm_ff
+import simtk
 
-__all__ = ["FFToParmedComponent", "ParmedToFFComponent"]
-
-bond_types = {
-    1: forcefield.bonded.bonds.potentials.Harmonic,
-    2: forcefield.bonded.bonds.potentials.Gromos96,
-}
-
-angle_types = {
-    1: forcefield.bonded.angles.potentials.Harmonic,
-}
-
-dihedral_types = {
-    1: forcefield.bonded.dihedrals.potentials.Charmm,
-    9: forcefield.bonded.dihedrals.potentials.CharmmMulti,
-}
-
-im_dihedral_types = {
-    4: ...,
-}
-
-# Need to fix: fudgeLJ, fudgeQQ
+__all__ = ["FFToOpenMMComponent", "OpenMMToFFComponent"]
 
 
-class FFToParmedComponent(TransComponent):
-    """A component for converting Molecule to ParmEd molecule object."""
+class FFToOpenMMComponent(TransComponent):
+    """A component for converting MMSchema to OpenMM ForceField object."""
 
     def execute(
         self,
@@ -43,11 +24,7 @@ class FFToParmedComponent(TransComponent):
         scratch_name: Optional[str] = None,
         timeout: Optional[int] = None,
     ) -> Tuple[bool, TransOutput]:
-        """
-        Works for writing PDB files e.g. pmol.save("file.pdb") but fails for gro files
-        TODO: need to investigate this more. Routine is also very slow. Try to vectorize.
-        Too many for loops. Can easily reduce these esp in the ParmedToFFComponent.
-        """
+
         if isinstance(inputs, dict):
             inputs = self.input()(**inputs)
 
@@ -248,7 +225,7 @@ class FFToParmedComponent(TransComponent):
     def _get_nonbonded(
         self,
         mmff: forcefield.ForceField,
-        empty_atom: parmed.topologyobjects.Atom,
+        empty_atom: "parmed.topologyobjects.Atom",
     ) -> Tuple["numpy.ndarray", "numpy.ndarray"]:
 
         assert (
@@ -270,8 +247,8 @@ class FFToParmedComponent(TransComponent):
         return rmin, epsilon
 
 
-class ParmedToFFComponent(TransComponent):
-    """A component for converting ParmEd molecule to Molecule object."""
+class OpenMMToFFComponent(TransComponent):
+    """A component for converting OpenMM ForceField to MMSchema object."""
 
     def execute(
         self,
@@ -288,57 +265,98 @@ class ParmedToFFComponent(TransComponent):
         ff = inputs.data_object
         mm_units = forcefield.ForceField.get_units()
 
+        atoms = ff._atomTypes
+        templates = ff._templates
+        templates_ = [
+            {resname: [atom.name for atom in residue.atoms]}
+            for resname, residue in templates.items()
+        ]
+
+        # names-defs = [(atom.name, symbols[atom.type]) for atom in residue.atoms for residue in templates.items()]
+
+        for gen in ff.getGenerators():
+            if isinstance(gen, simtk.openmm.app.forcefield.NonbondedGenerator):
+                nonbond_ff = gen
+            elif isinstance(gen, simtk.openmm.app.forcefield.HarmonicBondGenerator):
+                bond_ff = gen
+            elif isinstance(gen, simtk.openmm.app.forcefield.HarmonicAngleGenerator):
+                angle_ff = gen
+            elif isinstance(gen, simtk.openmm.app.forcefield.PeriodicTorsionGenerator):
+                dihedral_ff = gen
+            else:
+                raise NotImplementedError
+
         # Need to map these to potential types
         lj_units = forcefield.nonbonded.potentials.lenjones.LennardJones.get_units()
 
-        atom = ff.atoms[0]
-        bond = ff.bonds[0] if len(ff.bonds) else None
-        angle = ff.angles[0] if len(ff.angles) else None
-        dihedral = ff.dihedrals[0] if len(ff.dihedrals) else None
-
         data = [
             (
-                atom.name,
-                atom.type,
-                atom.charge,
+                atom.atomClass,
+                atom.element.symbol,
+                atom.element.atomic_number,
                 atom.mass,
-                atom.atomic_number,
-                atom.element_name,
             )
-            for atom in ff.atoms
+            for _, atom in ff._atomTypes.items()
         ]
-        names, types, charges, masses, atomic_numbers, elements = zip(*data)
-        charges = convert(
-            charges, atom.ucharge.unit.get_symbol(), mm_units["charges_units"]
-        )
-        masses = convert(masses, atom.umass.unit.get_symbol(), mm_units["masses_units"])
+        types, symbols, atomic_numbers, masses = zip(*data)
+        masses_units = atoms["0"].element.mass.unit.get_name()
+        masses = convert(masses, masses_units, mm_units["masses_units"])
 
-        rmin_factor = convert(1.0, atom.urmin.unit.get_name(), lj_units["sigma_units"])
-        rmin_14_factor = convert(
-            1.0, atom.urmin_14.unit.get_name(), lj_units["sigma_units"]
-        )
-        uepsilon_factor = convert(
-            1.0, atom.uepsilon.unit.get_name(), lj_units["epsilon_units"]
-        )
-        epsilon_14_factor = convert(
-            1.0, atom.uepsilon_14.unit.get_name(), lj_units["epsilon_units"]
-        )
-        scaling_factor = 1.0 / 2 ** (1.0 / 6.0)  # rmin = 2^(1/6) sigma
-        params = [
-            (
-                atom.rmin * rmin_factor * scaling_factor,
-                atom.rmin_14 * rmin_14_factor * scaling_factor,
-                atom.epsilon * uepsilon_factor,
-                atom.epsilon_14 * epsilon_14_factor,
-            )
-            for atom in ff.atoms
-        ]
-        sigma, sigma_14, epsilon, epsilon_14 = zip(*params)
+        # Non-bonded params
+        nb_params = self._get_nonbonded(nonbond_ff)
 
-        lj = forcefield.nonbonded.potentials.LennardJones(sigma=sigma, epsilon=epsilon)
+        lj = forcefield.nonbonded.potentials.LennardJones(
+            sigma=nb_params["sigma"], epsilon=nb_params["epsilon"]
+        )
         # Need to include sigma_14 and epsilon_14
         nonbonded = forcefield.nonbonded.NonBonded(params=lj, form="LennardJones")
 
+        # charge_groups = None ... should support charge_groups?
+        exclusions = None
+        inclusions = None
+
+        input_dict = {
+            "masses": masses,
+            "charges": nb_params["charge"],
+            # "bonds": bonds,
+            # "angles": angles,
+            # "dihedrals": dihedrals,
+            "nonbonded": nonbonded,
+            "exclusions": exclusions,
+            "inclusions": inclusions,
+            "defs": types,  # or names?
+            "symbols": symbols,
+            # "substructs": residues,
+            "atomic_numbers": atomic_numbers,
+        }
+
+        ff = forcefield.ForceField(**input_dict)
+        success = True
+        return success, TransOutput(
+            proc_input=inputs,
+            schema_object=ff,
+            success=success,
+            schema_version=inputs.schema_version,
+            schema_name=inputs.schema_name,
+        )
+
+    def _get_nonbonded(self, ff):
+        lj_scale = ff.lj14scale
+        coul_scale = ff.coulomb14scale
+
+        params = {}
+        # How to deal with lj/coul 1-4 scale? relevant to sigma/epsilon scale in parmed?
+
+        for paramName in ff.params.paramNames:
+            params[paramName] = [
+                ff.params.paramsForType[str(i)][paramName]
+                for i in range(len(ff.params.paramsForType))
+            ]
+
+        # How do we access units?
+        return params
+
+    def _get_bonded(self):
         if bond:
             bonds_units = (
                 forcefield.bonded.bonds.potentials.harmonic.Harmonic.get_units()
@@ -438,28 +456,6 @@ class ParmedToFFComponent(TransComponent):
 
         if hasattr(ff, "residues"):
             residues = [(atom.residue.name, atom.residue.idx) for atom in ff.atoms]
-
-        # charge_groups = None ... should support charge_groups?
-        exclusions = ff.nrexcl
-        inclusions = None
-
-        input_dict = {
-            "masses": masses,
-            "charges": charges,
-            "bonds": bonds,
-            "angles": angles,
-            "dihedrals": dihedrals,
-            "nonbonded": nonbonded,
-            "exclusions": exclusions,
-            "inclusions": inclusions,
-            "defs": types,  # or names?
-            "symbols": elements,
-            "substructs": residues,
-            "atomic_numbers": atomic_numbers,
-        }
-
-        ff = forcefield.ForceField(**input_dict)
-        return True, TransOutput(proc_input=inputs, schema_object=ff)
 
     def _get_dihedrals(self, funct, ff):
 
